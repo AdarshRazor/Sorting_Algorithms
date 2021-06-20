4ada7388c070945e6c42efd55bed1de6ec901916
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-20 16:14:17 +0530

    Create insertion_sort.cpp

---
+++ insertion_sort/insertion_sort.cpp
@@ -0,0 +1,45 @@
+// C++ program for insertion sort
+#include <bits/stdc++.h>
+using namespace std;
+
+/* we pick a key value and compare to its predecessor and sort it*/
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+
+        // decreasing value of j and comparing it to predecessors
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+        arr[j+1] = key;
+	}
+}
+
+// A utility function to print an array of size n
+void printArray(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+/* Driver code */
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	insertionSort(arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
+
+// This is code is contributed by rathbhupendra
