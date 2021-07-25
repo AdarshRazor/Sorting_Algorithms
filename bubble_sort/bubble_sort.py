41df5c86f31c07b36a2c9023e92095a6677d6125
Adarsh Anand <adarshanshu7@gmail.com>
2021-07-26 04:38:01 +0530

    Create bubble_sort.py

---
+++ bubble_sort/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [27,13,57,43,98,3,7,11,45,21,100]
+
+print(bubble_sort(arr))
